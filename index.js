32a08eb03b624dded4a764523a0fe9b3c89a9b0b
Jeet267 <152002098+Jeet267@users.noreply.github.com>
2024-11-07 22:48:54 +0530

    Update index.js
    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
+// const express=  require('express');
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors'
+import dotenv from "dotenv"
+import dbConnect from './utils/db.js';
+import userRoute from './routes/user.routes.js'
+import companyRoute from './routes/company.routes.js'
+import jobRoute from './routes/job.routes.js'
+import applicationRoute from './routes/application.routes.js'
 
 dotenv.config({});
 
 const app = express();
 
-// middleware
-app.use(express.json());
+app.get("/home",(req,res)=>{
+    return res.status(400).json({
+        message:"I am frontend",
+        success :true
+    })
+
+})
+
+app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
+const corsOptions ={
+    origin:'https//localhost:5173',
+    credential :true
 }
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests only from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    credentials: true, // Include credentials like cookies if needed
+  }));
+  
 
-app.use(cors(corsOptions));
+const PORT = process.env.Port || 3000;
 
-const PORT = process.env.PORT || 3001;
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/company",companyRoute);
+app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/application",applicationRoute);
 
-
-// api's
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute);
-app.use("/api/v1/application", applicationRoute);
-
-
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
+app.listen(PORT, () =>{
+    dbConnect()
+    console.log(`Server running att port ${PORT}`)
 })
