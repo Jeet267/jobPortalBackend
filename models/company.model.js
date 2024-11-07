f92c0748f3647d11a5c84ead7b4d736bd7b4879c
Jeet267 <152002098+Jeet267@users.noreply.github.com>
2024-11-07 22:45:24 +0530

    Update company.model.js
    Update company.model.js

--- models/company.model.js
+++ models/company.model.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-
 const companySchema = new mongoose.Schema({
     name:{
         type:String,
@@ -7,21 +6,25 @@ const companySchema = new mongoose.Schema({
         unique:true
     },
     description:{
-        type:String, 
+        type:String,
     },
     website:{
-        type:String 
+        type:String,
     },
     location:{
-        type:String 
+        type:String,
+
     },
     logo:{
-        type:String // URL to company logo
+        type:String,
     },
     userId:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User',
         required:true
-    }
+    },
+
+
 },{timestamps:true})
-export const Company = mongoose.model("Company", companySchema);
+export default  mongoose.model("Company",companySchema)
+// export const Company = mongoose.model('Company', companySchema);
