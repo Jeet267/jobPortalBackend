dd5f92ab09fc8371b94c84f0fc03b23fcd244c5d
Jeet267 <152002098+Jeet267@users.noreply.github.com>
2024-11-07 22:47:54 +0530

    Update job.route.js
    Update job.route.js

--- routes/job.route.js
+++ routes/job.route.js
@@ -1,13 +1,12 @@
-import express from "express";
-import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { getAdminJobs, getAllJobs, getJobById, postJob } from "../controllers/job.controller.js";
-
+import express from 'express';
+// import { register,login, updateProfile,logout} from '../controllers/user.controller.js';
+import isAuthenticated from '../middlewares/isAuthenticated.js'
+import { getAllJobs, postJob , getJobById,getAdminJobs} from '../controllers/job.controllers.js';
 const router = express.Router();
-
-router.route("/post").post(isAuthenticated, postJob);
-router.route("/get").get(isAuthenticated, getAllJobs);
-router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
-router.route("/get/:id").get(isAuthenticated, getJobById);
-
+router.route("/post").post(isAuthenticated,postJob);
+router.route("/get").get(isAuthenticated,getAllJobs);
+router.route("/get/:id").get(isAuthenticated,getJobById)
+router.route("/getAdminJobs").get(isAuthenticated,getAdminJobs)
 export default router;
+
 
